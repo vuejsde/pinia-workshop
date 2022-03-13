db1bac40f4400894cc643e3036e8ea6fb0034882
Antony Konstantinidis <antony@vuejs.de>
2022-03-13 18:40:03 +0100

    Install and setup pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
 import router from './router';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
+app.use(createPinia());
 app.mount('#app');
