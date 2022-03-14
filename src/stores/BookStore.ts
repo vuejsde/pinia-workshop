87963be7e74a40398dfdf68c33a35b4de486fe75
Antony Konstantinidis <antony@vuejs.de>
2022-03-14 10:25:06 +0100

    Accessing other stores

--- src/stores/BookStore.ts
+++ src/stores/BookStore.ts
@@ -1,6 +1,7 @@
-import { defineStore } from 'pinia';
-import { get } from '@/utils/http';
+import { defineStore, storeToRefs } from 'pinia';
+import { get, post } from '@/utils/http';
 import type { Book } from '@/types';
+import { useAuthStore } from './AuthStore';
 
 export type BookState = {
   books: Array<Book>;
@@ -17,6 +18,22 @@ export const useBookStore = defineStore('BookStore', {
       const books = await get<Book[]>('http://localhost:4730/books');
 
       this.books = books;
+    },
+    async create(book: Partial<Book>) {
+      const { user } = storeToRefs(useAuthStore());
+      const newBook = await post<Book>(
+        `http://localhost:4730/books`,
+        {
+          ...book,
+          userId: user.value?.id,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      this.books.push(newBook);
     },
   },
 });
